0447104bd84cbfd50b918b9b0011e466454bed90
Rowins Bie <rowins.bie.srb@gmail.com>
2023-05-09 20:32:43 +0800

    created nav bar component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script  lang="ts">
 import { RouterView } from 'vue-router';
 import {defineComponent} from 'vue';
+
+// components
+import NavBar from './components/NavBar.vue';
+
 export default  defineComponent({
   components:{
-    RouterView
+    RouterView,
+    NavBar
   }  
 })
 </script>
 
 <template>
   <div>
+    <NavBar />
     <RouterView></RouterView>
   </div>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template lang="">
+    
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="https://flowbite.com" class="flex items-center">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SCredit MS</span>
+        </a>
+        <div class="flex items-center">
+            <a href="tel:5541251234" class="mr-6 text-sm  text-gray-500 dark:text-white hover:underline">cart here
+            </a>
+            <a href="#" class="text-sm  text-blue-600 dark:text-white bg-blue-800 py-2 px-3 rounded-lg ">Login</a>
+        </div>
+    </div>
+</nav>
+<nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto">
+        <div class="flex items-center">
+            <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue';
+export default defineComponent({
+    
+})
+</script>
+<style lang="">
+    
+</style>
