488eaa6b92cd68428b49b9eab685b7700edff9a5
Rowins Bie <rowins.bie.srb@gmail.com>
2023-05-09 23:17:24 +0800

    added comments

--- src/router.ts
+++ src/router.ts
@@ -13,17 +13,47 @@ const routes : Array<RouteRecordRaw> = [
     {
         path:"/login",
         name:"Login",
-        component:LoginPage
+        component:LoginPage,
+        meta: { requiresAuth: false },
+       /* it is checking if the route being accessed requires authentication
+       (`to.meta.requiresAuth`) and if the user has a valid token
+       (`localStorage.getItem('sanctum-token') !== null`). If both conditions are true, it redirects
+       the user to the dashboard page (`next({name:'Dashboard'})`). Otherwise, it allows the user to
+       proceed to the requested page (`next()`). */
+        beforeEnter:(to,from,next) => {
+            if(!to.meta.requiresAuth && localStorage.getItem('sanctum-token') !== null) {
+                return next({name:'Dashboard'})
+            } else {
+                next();
+            }
+        }
+
     },
     {
         path:'/dashboard',
-        name:"dashboard",
-        component:DashboardPage
+        name:"Dashboard",
+        component:DashboardPage,
+        meta:{
+            requiresAuth:true
+        }
     }
 ]
 const router = createRouter({
     history:createWebHashHistory(),
     routes
 });
+
+
+router.beforeEach((to,from,next) => {
+   /* This code is a navigation guard that is executed before each navigation. It checks if the user is
+   authenticated by verifying if there is a valid token in the local storage
+   (`localStorage.getItem('sanctum-token')`). If the user is not authenticated and is trying to
+   access a page other than the login page (`to.name !== 'Login'`), it redirects the user to the
+   login page (`next({ name: 'Login' })`). Otherwise, it allows the user to proceed to the requested
+   page (`else next()`). */
+    if (to.name !== 'Login' && !localStorage.getItem('sanctum-token')) next({ name: 'Login' })
+    else next()
+   
+})
 
 export default router;
