263a4777a89c1db51f41deb2cf80632d00c182b6
Rowins Bie <rowins.bie.srb@gmail.com>
2023-05-11 21:32:48 +0800

    redesigned header

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template lang="">
     
 <div class="fixed top-0 left-0 right-0 z-50">
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <nav class="bg-white border-gray-200 dark:bg-gradient-to-r from-slate-800 to-slate-900">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl  p-4">
             <RouterLink to="/" class="flex items-center">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-green-500">SCredit MS</span>
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sari-sari Store Digital</span>
             </RouterLink>
             <div class="flex items-center">
-                <RouterLink to="/cart" class="mr-6 text-sm  text-gray-500 dark:text-white hover:underline hover:bg-violet-700 px-1 rounded-full "><i class='bx bxs-cart-alt text-3xl '></i>
+                <RouterLink to="/cart" class="mr-6 text-sm  text-gray-500 dark:text-white hover:underline hover:bg-gray-700 px-1 rounded-full "><i class='bx bxs-cart-alt text-3xl '></i>
     
                 </RouterLink>
                 <RouterLink to="/login" class="text-sm  text-blue-600 dark:text-white  py-2 px-3 rounded-lg "><i class='bx bxs-user-circle text-3xl'></i> </RouterLink>
@@ -15,7 +15,7 @@
             </div>
         </div>
     </nav>
-    <nav class="bg-gray-50 dark:bg-gray-50 border">
+    <nav class="bg-gray-50 dark:white ">
         <div class="max-w-screen-xl px-4 py-3 mx-auto">
             <div class="flex items-center">
                 <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
